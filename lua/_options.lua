e3484be30eda8f4c47820e298b4f7a0c0a9f699a
Colten Begle <coltenbegle@icloud.com>
2022-06-11 09:14:15 -0400

    Added term package

--- lua/_options.lua
+++ lua/_options.lua
@@ -35,7 +35,23 @@ vim.o.completeopt        = "menuone,noinsert,noselect"
 vim.o.wildmode           = "longest,full"                -- Display auto-complete in Command Mode
 -- vim.o.updatetime         = 300                           -- Delay until write to Swap and HoldCommand event
 
-vim.cmd[[colorscheme onedark]]
+-- vim.cmd[[colorscheme onedark]]
+-- Lua:
+-- For dark theme (neovim's default)
+vim.o.background = "dark"
+-- For light theme
+-- vim.o.background = "light"
+-- Enable transparent background
+vim.g.vscode_transparent = 1
+-- Enable italic comment
+vim.g.vscode_italic_comment = 1
+-- Disable nvim-tree background color
+vim.g.vscode_disable_nvimtree_bg = true
+vim.cmd([[colorscheme vscode]])
 
 require('nvim-autopairs').setup{}
 
+require('toggleterm').setup{}
+
+
+require('lualine').setup{}
--- lua/_packer.lua
+++ lua/_packer.lua
@@ -1,6 +1,6 @@
 return require('packer').startup(function()
-	use 'wbthomason/packer.nvim' -- Package manager
-	use 'neovim/nvim-lspconfig'
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
@@ -10,22 +10,27 @@ return require('packer').startup(function()
 
   use 'windwp/nvim-autopairs'
 
-	use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = ':TSUpdate'
-    	}
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'kyazdani42/nvim-web-devicons'
+  use 'BurntSushi/ripgrep'
+  use 'sharkdp/fd'
 
-	-- Telescope
-	use {
-	  'nvim-telescope/telescope.nvim',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use 'kyazdani42/nvim-web-devicons'
-	use 'BurntSushi/ripgrep'
-	use 'sharkdp/fd'
+  -- Color schemes
+  use 'navarasu/onedark.nvim'
+  use 'Mofiqul/vscode.nvim'
 
-	-- Color schemes
-	use 'navarasu/onedark.nvim'
-	
+  -- Terminal
+  use {"akinsho/toggleterm.nvim", tag = 'v1.*', config = function()
+    require("toggleterm").setup()
+  end}
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end)
 
